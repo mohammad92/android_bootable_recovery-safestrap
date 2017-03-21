28e65177d9997f8dff89a9e22cd073f79bb4e3b5
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2017-03-21 03:48:20 +0200

    safestrap v4.05

--- variables.h
+++ variables.h
@@ -21,7 +21,7 @@
 #define TW_VERSION_STR TW_MAIN_VERSION_STR TW_DEVICE_VERSION
 
 #ifdef BUILD_SAFESTRAP
-#define SS_VERSION_STR              "4.04"
+#define SS_VERSION_STR              "4.05"
 #endif
 #define TW_USE_COMPRESSION_VAR      "tw_use_compression"
 #define TW_FILENAME                 "tw_filename"
