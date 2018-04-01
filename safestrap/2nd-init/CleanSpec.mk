7fae3418f5a7826921e3ca60b927672173aa80a6
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2018-04-01 14:24:41 +0300

    Add clean step to 2nd-init, Update splashmenu one

--- safestrap/2nd-init/CleanSpec.mk
+++ safestrap/2nd-init/CleanSpec.mk
@@ -44,8 +44,8 @@
 # $(call add-clean-step, find $(OUT_DIR) -type f -name "IGTalkSession*" -print0 | xargs -0 rm -f)
 # $(call add-clean-step, rm -rf $(PRODUCT_OUT)/data/*)
 
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/RECOVERY_EXECUTABLES/safestrapmenu_intermediates)
-$(call add-clean-step, rm $(PRODUCT_OUT)/install-files/etc/safestrap/safestrapmenu)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/RECOVERY_EXECUTABLES/2nd-init_intermediates)
+$(call add-clean-step, rm $(PRODUCT_OUT)/2nd-init-files/2nd-init)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
