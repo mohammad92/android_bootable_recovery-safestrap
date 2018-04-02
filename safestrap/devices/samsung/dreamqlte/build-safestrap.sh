3f0ea5a97e2aa1516fc3eebb60df8849b218c924
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2018-04-02 22:44:37 +0300

    safestrap: Add initial Samsung Galaxy S8/S8+ (Snapdragon) (dreamqlte/dream2qlte) files

---
+++ safestrap/devices/samsung/dreamqlte/build-safestrap.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/msm8998-common/build-safestrap.sh
+
---
+++ safestrap/devices/samsung/dreamqlte/safestrap.mk
@@ -0,0 +1,4 @@
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/common/safestrap-common.mk
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/samsung/msm8998-common/safestrap-msm8998-common.mk
+
+SS_TWRP_PRINT_SCREENINFO := true
