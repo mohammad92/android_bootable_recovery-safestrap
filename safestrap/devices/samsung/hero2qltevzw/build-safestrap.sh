0bfd8710fb1271d22b3043c78b7ec9a8d0b37b29
klabit87 <keith.labit@gmail.com>
2018-04-07 14:43:59 -0500

    Add heroqltevzw & hero2qltevzw support

---
+++ safestrap/devices/samsung/hero2qltevzw/build-safestrap.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/hero-common/build-safestrap.sh
+
---
+++ safestrap/devices/samsung/hero2qltevzw/safestrap.mk
@@ -0,0 +1,4 @@
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/common/safestrap-common.mk
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/samsung/hero-common/safestrap-hero-common.mk
+
+SS_TWRP_PRINT_SCREENINFO := true
