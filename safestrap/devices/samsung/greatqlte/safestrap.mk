f58d8d8e8d75213bc33be8f9f1c957cea0d129f4
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2018-04-07 23:02:05 +0300

    greatqlte: Remove TWRP_EVENT_LOGGING := false

--- safestrap/devices/samsung/greatqlte/safestrap.mk
+++ safestrap/devices/samsung/greatqlte/safestrap.mk
@@ -2,7 +2,6 @@ include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/common/safestra
 include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/samsung/msm8998-common/safestrap-msm8998-common.mk
 
 SS_TWRP_PRINT_SCREENINFO := true
-TWRP_EVENT_LOGGING := false
 
 # Virtual partition size default (in mb)
 BOARD_DEFAULT_VIRT_SYSTEM_SIZE := 5120
