00dab216701fd69b1d1cff00e625c0c12cc7dc53
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2018-08-11 10:57:23 +0300

    safestrap: Add script to reboot back to safestrap recovery

---
+++ safestrap/devices/common/sbin/rebootrecovery.sh
@@ -0,0 +1,13 @@
+#!/sbin/bbx sh
+# By: Mohammad Afaneh, Afaneh92@xda
+# wrapper to reboot back to Safestrap recovery
+PATH=/sbin:/system/xbin:/system/bin
+
+BBX=/sbin/bbx
+SS_CONFIG=/ss.config
+
+. /sbin/ss_function.sh
+readConfig
+
+$BBX echo 1 > /data/$SS_RECOVERY_FILE
+$BBX reboot now
