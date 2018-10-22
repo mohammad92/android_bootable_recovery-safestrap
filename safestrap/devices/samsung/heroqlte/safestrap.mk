3a6004bea8c687eba61aec96a63541766faf2bb8
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2018-10-22 11:00:17 +0300

    safestrap: Add initial qcomm S7/S7Edge support

---
+++ safestrap/devices/samsung/heroqlte/safestrap.mk
@@ -0,0 +1,4 @@
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/common/safestrap-common.mk
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/samsung/msm8996-common/safestrap-msm8996-common.mk
+
+SS_TWRP_PRINT_SCREENINFO := true
