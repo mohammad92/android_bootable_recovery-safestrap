d4e02b9dfe516d5d8d16f710006de1b923150b54
Captain Throwback <captainthrowback@hotmail.com>
2019-02-07 17:22:49 -0500

    mksh: add mkshrc when using TW_USE_TOOLBOX
    Fixes broken shell prompt in Terminal and adb shell
    
    Before: https://del.dog/7vphlkc
    After: https://del.dog/vhkjgo8
    
    Add as prebuilt with suffix "_twrp" to avoid conflict
    with original mkshrc and rename via POST_INSTALL_CMD
    
    Prebuilt version adjusts TMPDIR variable from
    "/data/local/tmp" to "/tmp" for TWRP
    
    Change-Id: I42a1b2a8c114f66dbe659c07485f1dfae8a5d314

--- etc/Android.mk
+++ etc/Android.mk
@@ -113,3 +113,18 @@ LOCAL_SRC_FILES := init.recovery.safestrap21.rc
 endif
 include $(BUILD_PREBUILT)
 endif
+
+ifeq ($(TW_USE_TOOLBOX), true)
+    include $(CLEAR_VARS)
+    LOCAL_MODULE := init.recovery.mksh.rc
+    LOCAL_MODULE_TAGS := eng
+    LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+
+    # Cannot send to TARGET_RECOVERY_ROOT_OUT since build system wipes init*.rc
+    # during ramdisk creation and only allows init.recovery.*.rc files to be copied
+    # from TARGET_ROOT_OUT thereafter
+    LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+
+    LOCAL_SRC_FILES := $(LOCAL_MODULE)
+    include $(BUILD_PREBUILT)
+endif
