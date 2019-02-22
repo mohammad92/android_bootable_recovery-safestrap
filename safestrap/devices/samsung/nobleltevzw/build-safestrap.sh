98e8a02afed986173757bd3ae5846fc8d650e3ec
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2019-02-22 21:20:38 +0200

    Add initial Note 5 (nobleltevzw) suppoert

---
+++ safestrap/devices/samsung/nobleltevzw/build-safestrap.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sh $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/samsung/exynos7420-common/build-safestrap.sh
---
+++ safestrap/devices/samsung/nobleltevzw/safestrap.mk
@@ -0,0 +1,2 @@
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/common/safestrap-common.mk
+include $(ANDROID_BUILD_TOP)/bootable/recovery/safestrap/devices/samsung/exynos7420-common/safestrap-exynos7420-common.mk
