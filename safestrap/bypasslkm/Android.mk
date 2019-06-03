a10d6c7c08888bf06e336ae518a36a1e146af915
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2019-06-03 20:57:34 +0300

    bypasslkm: Disable relocations, clean up and fix build on 8.0+

--- safestrap/bypasslkm/Android.mk
+++ safestrap/bypasslkm/Android.mk
@@ -1,13 +1,14 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
+LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= bypasslkm.c
 LOCAL_STATIC_LIBRARIES += libc
-
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE := bypasslkm
 
 LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+LOCAL_PACK_MODULE_RELOCATIONS := false
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_OUT)/../install-files/$(SS_LOC)/safestrap
+LOCAL_UNSTRIPPED_PATH := $(TARGET_RECOVERY_OUT)/../symbols/install-files/$(SS_LOC)/safestrap
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
