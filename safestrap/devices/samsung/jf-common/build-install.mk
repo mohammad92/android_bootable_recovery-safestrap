6b3410221c6b7e7a8e0d660a9b41926cb73da7ed
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2019-06-03 22:48:37 +0300

    jf-common: Add hacked init binary

--- safestrap/devices/samsung/jf-common/build-install.mk
+++ safestrap/devices/samsung/jf-common/build-install.mk
@@ -1,3 +1,4 @@
 # remove fstab.qcom from recovery
 INSTALL_SAFESTRAP_DEVICE_CMD := \
-	rm -f $(TARGET_RECOVERY_ROOT_OUT)/fstab.qcom;
+	rm -f $(TARGET_RECOVERY_ROOT_OUT)/fstab.qcom; \
+	cp $(SS_COMMON)/devices/samsung/jf-common/rootfs/init $(TARGET_RECOVERY_ROOT_OUT)/;
