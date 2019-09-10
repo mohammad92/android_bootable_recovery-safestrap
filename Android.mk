18a5b21138b716fddaeabad8233ce1dccb642b43
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2019-09-10 11:39:06 +0300

    safestrap: Add check for making sure safestrap.mk exists for device

--- Android.mk
+++ Android.mk
@@ -29,7 +29,26 @@ ifeq ($(BUILD_SAFESTRAP), true)
 ifeq ($(SS_PRODUCT_MANUFACTURER), )
     SS_PRODUCT_MANUFACTURER := $(shell echo $(PRODUCT_MANUFACTURER) | tr '[A-Z]' '[a-z]')
 endif
-include $(commands_TWRP_local_path)/safestrap/devices/$(SS_PRODUCT_MANUFACTURER)/$(TARGET_DEVICE)/safestrap.mk
+
+SS_PRODUCT_MAKEFILE_LOC := $(commands_TWRP_local_path)/safestrap/devices/$(SS_PRODUCT_MANUFACTURER)/$(TARGET_DEVICE)
+
+# The extra blank line before *** is intentional to ensure it ends up on its own line
+define SS_MAKEFILE_WARNING_MSG
+
+****************************************************************************
+  Expected to find safestrap.mk at:
+    $(SS_PRODUCT_MAKEFILE_LOC)
+  Please fix this or set $(SS_PRODUCT_MANUFACTURER)/$(TARGET_DEVICE) to the following:
+    /safestrap/devices/
+****************************************************************************
+endef
+
+ifeq ($(wildcard $(SS_PRODUCT_MAKEFILE_LOC)/safestrap.mk),)
+    $(warning $(SS_MAKEFILE_WARNING_MSG))
+    $(error safestrap.mk selection failed; exiting)
+else
+    include $(SS_PRODUCT_MAKEFILE_LOC)/safestrap.mk
+endif
 endif
 
 ifneq ($(project-path-for),)
