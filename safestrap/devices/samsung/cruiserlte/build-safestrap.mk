7c3484a0c0195bd9364527ff20636a2acdcad87a
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2019-10-27 20:21:47 +0200

    cruiserlte: Fix typo

--- safestrap/devices/samsung/cruiserlte/build-safestrap.mk
+++ safestrap/devices/samsung/cruiserlte/build-safestrap.mk
@@ -4,4 +4,4 @@ BUILD_SAFESTRAP_CMD += && \
 	cp -fr $(SS_COMMON)/devices/samsung/cruiserlte/ss.config $(PRODUCT_OUT)/APP/ss.config && \
 	cp -fr $(SS_COMMON)/devices/samsung/cruiserlte/ss.config $(PRODUCT_OUT)/install-files/$(SS_LOC)/safestrap/ss.config && \
 	cp -fr $(SS_COMMON)/devices/samsung/cruiserlte/ss.config $(TARGET_RECOVERY_ROOT_OUT)/ss.config && \
-	cp -fr $(SS_COMMON)/devices/samsung/cruiserlte/twrp.fstab $(TARGET_RECOVERY_ROOT_OUT)/etc/recovery.fstab && \
+	cp -fr $(SS_COMMON)/devices/samsung/cruiserlte/twrp.fstab $(TARGET_RECOVERY_ROOT_OUT)/etc/recovery.fstab
