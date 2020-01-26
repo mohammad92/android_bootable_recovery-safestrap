6622d3fb575b5024eb9eea34a91c4bbf6d4a250d
Chaosmaster <chaosmaster@chaosv.de>
2020-01-27 00:17:04 +0100

    Implement autodetection for SAR, based on the installed system
    This will detect whether SAR is present in the currently installed
    OS and set the property ro.twrp.sar accordingly.
    After setting the property it will call the bootscript
    /sbin/sarsetup.sh (if present) to give device maintainers the
    option to do setup operations depending on SAR-status, such
    as modifiyng the fstab.
    
    If no system is detected and AB_OTA_UPDATER is defined or built with
    Android 10 and upwards, it will fallback to using SAR, otherwise it
    will use ro.build.system_root_image as basis for deciding whether SAR
    is required or not.
    
    The property ro.twrp.sar will also be used by
    TWPartitionManager::Get_Android_Root_Path()
    
    This allows maintaining a single TWRP-build for devices switching
    to SAR for Android 10.
    
    The default behavior (when no system is installed)
    is determined by the build-flags AB_OTA_UPDATER and
    BOARD_BUILD_SYSTEM_ROOT_IMAGE
    
    Change-Id: I2a48c6c81a6ea6fad6e452c06bfbe4d9da0f1e5c

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -112,9 +112,9 @@ TWPartitionManager::TWPartitionManager(void) {
 }
 
 #ifdef BUILD_SAFESTRAP
-int TWPartitionManager::Process_Fstab(string Fstab_Filename, bool Display_Error, bool Reset_Partition_List) {
+int TWPartitionManager::Process_Fstab(string Fstab_Filename, bool Display_Error, bool Sar_Detect, bool Reset_Partition_List) {
 #else
-int TWPartitionManager::Process_Fstab(string Fstab_Filename, bool Display_Error) {
+int TWPartitionManager::Process_Fstab(string Fstab_Filename, bool Display_Error, bool Sar_Detect) {
 #endif
 	FILE *fstabFile;
 	char fstab_line[MAX_FSTAB_LINE_LENGTH];
@@ -207,7 +207,7 @@ int TWPartitionManager::Process_Fstab(string Fstab_Filename, bool Display_Error)
 			fstab_line[line_size] = '\n';
 
 		TWPartition* partition = new TWPartition();
-		if (partition->Process_Fstab_Line(fstab_line, Display_Error, &twrp_flags))
+		if (partition->Process_Fstab_Line(fstab_line, Display_Error, &twrp_flags, Sar_Detect))
 			Partitions.push_back(partition);
 		else
 			delete partition;
@@ -222,7 +222,7 @@ int TWPartitionManager::Process_Fstab(string Fstab_Filename, bool Display_Error)
 		for (std::map<string, Flags_Map>::iterator mapit=twrp_flags.begin(); mapit!=twrp_flags.end(); mapit++) {
 			if (Find_Partition_By_Path(mapit->first) == NULL) {
 				TWPartition* partition = new TWPartition();
-				if (partition->Process_Fstab_Line(mapit->second.fstab_line, Display_Error, NULL))
+				if (partition->Process_Fstab_Line(mapit->second.fstab_line, Display_Error, NULL, Sar_Detect))
 					Partitions.push_back(partition);
 				else
 					delete partition;
@@ -236,6 +236,13 @@ int TWPartitionManager::Process_Fstab(string Fstab_Filename, bool Display_Error)
 
 	std::vector<TWPartition*>::iterator iter;
 	for (iter = Partitions.begin(); iter != Partitions.end(); iter++) {
+		if (Sar_Detect) {
+			if ((*iter)->Mount_Point == "/s")
+				return true;
+			else
+				continue;
+		}
+
 		(*iter)->Partition_Post_Processing(Display_Error);
 
 		if ((*iter)->Is_Storage) {
@@ -3009,7 +3016,7 @@ string TWPartitionManager::Get_Active_Slot_Display() {
 }
 
 string TWPartitionManager::Get_Android_Root_Path() {
-	if (property_get_bool("ro.build.system_root_image", false))
+	if (property_get_bool("ro.twrp.sar", false))
 		return "/system_root";
 	return "/system";
 }
