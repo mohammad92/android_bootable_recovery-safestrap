TeamWin/android_bootable_recovery

Fix up cache wipe issue in A-Only device
 * missed in https://github.com/TeamWin/android_bootable_recovery/commit/3754fabbea2eefeb7c1a0c5bc804f1e14a686855

Change-Id: I8fc3719e7f98c3d9b5fd96a9820b84773d4f5d7d
Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

partition.cpp
@@ -1706,6 +1706,9 @@ bool TWPartition::Wipe(string New_File_System) {
 	}
 
 	if (wiped) {
+		if (Mount_Point == "/cache" && TWFunc::get_cache_dir() != AB_CACHE_DIR)
+			DataManager::Output_Version();
+
 		if (TWFunc::Path_Exists("/.layout_version") && Mount(false))
 			TWFunc::copy_file("/.layout_version", Layout_Filename, 0600);
 

mohammad92/android_bootable_recovery-safestrap

partition.cpp
@@ -1758,6 +1758,9 @@ bool TWPartition::Wipe(string New_File_System) {
 	}
 
 	if (wiped) {
+		if (Mount_Point == "/cache" && TWFunc::get_log_dir() != DATA_LOGS_DIR)
+			DataManager::Output_Version();
+
 		if (TWFunc::Path_Exists("/.layout_version") && Mount(false))
 			TWFunc::copy_file("/.layout_version", Layout_Filename, 0600);
 