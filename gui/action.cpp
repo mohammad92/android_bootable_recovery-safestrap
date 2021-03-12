dc16ab4f4049c318aec587661142584c6123b4c6
epicX <souravgain605@gmail.com>
2021-03-12 18:14:23 +0530

    Allow all extension while checking for digest verification
    Change-Id: Ie17582f144e0a5b5a68e2c6d88c26cb9f5a29ee6

--- gui/action.cpp
+++ gui/action.cpp
@@ -421,7 +421,7 @@ int GUIAction::flash_zip(std::string filename, int* wipe_cache)
 #ifdef BUILD_SAFESTRAP
 		if (PartitionManager.Backup_Safestrap()) return 1;
 #endif
-		ret_val = TWinstall_zip(filename.c_str(), wipe_cache);
+		ret_val = TWinstall_zip(filename.c_str(), wipe_cache, (bool) DataManager::GetIntValue(TW_SKIP_DIGEST_CHECK_VAR));
 
 		// Now, check if we need to ensure TWRP remains installed...
 		struct stat st;
