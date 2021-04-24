TeamWin/android_bootable_recovery

Add support to change TWRP Folder
Change-Id: Id3b3b46d492bf83d1d9d3f535c880ea9d15b4107

gui/objects.hpp
@@ -371,6 +371,7 @@ protected:
 	int uninstalltwrpsystemapp(std::string arg);
 	int repackimage(std::string arg);
 	int fixabrecoverybootloop(std::string arg);
+	int apply_CustomTWRPFolder(std::string arg);
 
 	int simulate;
 };

twrp.cpp
@@ -330,6 +330,7 @@ int main(int argc, char **argv) {
 
 	// Read the settings file
 	TWFunc::Update_Log_File();
+	DataManager::LoadTWRP_FolderInfo();
 	DataManager::ReadSettingsFile();
 	PageManager::LoadLanguage(DataManager::GetStrValue("tw_language"));
 	GUIConsole::Translate_Now();

mohammad92/android_bootable_recovery-safestrap

gui/objects.hpp
@@ -375,6 +375,7 @@ protected:
 #ifndef TW_EXCLUDE_NANO
 	int editfile(std::string arg);
 #endif
+	int applycustomtwrpfolder(std::string arg);
 
 #ifdef BUILD_SAFESTRAP	
         // safestrap threaded actions

twrp.cpp
@@ -338,6 +338,7 @@ int main(int argc, char **argv) {
 
 	// Read the settings file
 	TWFunc::Update_Log_File();
+	DataManager::LoadTWRPFolderInfo();
 	DataManager::ReadSettingsFile();
 	PageManager::LoadLanguage(DataManager::GetStrValue("tw_language"));
 	GUIConsole::Translate_Now();