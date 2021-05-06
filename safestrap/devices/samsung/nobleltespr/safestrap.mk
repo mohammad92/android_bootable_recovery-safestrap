55c78336064a853acd59c8eef155b17d8ac7041b
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2021-05-07 01:40:04 +0300

    Add initial Note 5 (nobleltespr) support

---
+++ safestrap/devices/samsung/nobleltespr/safestrap.mk
@@ -0,0 +1,7 @@
+include $(commands_TWRP_local_path)/safestrap/devices/common/safestrap-common.mk
+include $(commands_TWRP_local_path)/safestrap/devices/samsung/exynos7420-common/safestrap-exynos7420-common.mk
+
+# Virtual partition size default (in mb)
+BOARD_DEFAULT_VIRT_SYSTEM_SIZE := 4320
+BOARD_DEFAULT_VIRT_SYSTEM_MIN_SIZE := 4320
+BOARD_DEFAULT_VIRT_SYSTEM_MAX_SIZE := 4320
